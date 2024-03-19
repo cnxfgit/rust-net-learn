c4ce7ad284e32b35edc35edf348aaba5f47c94b4
hlx <1198816371@qq.com>
2024-03-19 21:23:32 +0800

    build

---
+++ use_build/build.rs
@@ -0,0 +1,13 @@
+use std::{env, fs::File, io::Write, path::Path};
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hello.rs");
+    let mut f = File::create(&dest_path).unwrap();
+
+    f.write_all(b"
+        fn say_hello() -> &'static str{
+            \"hello\"
+        }
+    ").unwrap();
+}
